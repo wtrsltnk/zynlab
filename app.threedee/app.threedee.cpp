784ce369be0aa061842f9815d99a05d41b5e0b4e
Wouter Saaltink <wouter.saaltink@gmail.com>
2017-06-17 12:32:03 +0200

    added child windows

--- app.threedee/app.threedee.cpp
+++ app.threedee/app.threedee.cpp
@@ -75,153 +75,164 @@ void AppThreeDee::Render()
     ImGui_ImplGlfwGL3_NewFrame();
 
     ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 1.0f);
-
-    if (ImGui::BeginMainMenuBar())
+    ImGui::PushStyleColor(ImGuiCol_WindowBg, ImVec4(1.0f, 1.0f, 1.0f, 0.0f));
     {
-        if (ImGui::BeginMenu("File"))
+        if (ImGui::BeginMainMenuBar())
         {
-//            ShowExampleMenuFile();
-            ImGui::EndMenu();
+            if (ImGui::BeginMenu("File"))
+            {
+    //            ShowExampleMenuFile();
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("Edit"))
+            {
+                if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+                if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+                ImGui::Separator();
+                if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+                if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+                if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+                ImGui::EndMenu();
+            }
+            ImGui::EndMainMenuBar();
         }
-        if (ImGui::BeginMenu("Edit"))
+
+        static bool show_library = false;
+        static bool show_inspector = false;
+        static bool show_smartcontrols = false;
+        static bool show_editors = false;
+        static bool show_mixer = false;
+        static bool open = true;
+
+        int leftWidth = (this->_display_w - 210) / 2;
+        ImGui::Begin("TOOLBAR_LEFT", &open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove |  ImGuiWindowFlags_NoTitleBar);
         {
-            if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
-            if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
-            ImGui::Separator();
-            if (ImGui::MenuItem("Cut", "CTRL+X")) {}
-            if (ImGui::MenuItem("Copy", "CTRL+C")) {}
-            if (ImGui::MenuItem("Paste", "CTRL+V")) {}
-            ImGui::EndMenu();
-        }
-        ImGui::EndMainMenuBar();
-    }
+            ImGui::SetWindowPos(ImVec2(0, 22));
+            ImGui::SetWindowSize(ImVec2(leftWidth, 40));
+            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(5,15));
 
-    static bool show_library = true;
-    static bool show_inspector = true;
-    static bool show_smartcontrols = true;
-    static bool show_editors = true;
-    static bool show_mixer = true;
-    static bool open = true;
+            if (ImGui::Button(FontAwesomeIcons::FA_INBOX, ImVec2(30, 30))) show_library ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(FontAwesomeIcons::FA_INFO, ImVec2(30, 30))) show_inspector ^= 1;
 
-    int leftWidth = (this->_display_w - 210) / 2;
-    ImGui::Begin("TOOLBAR_LEFT", &open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove |  ImGuiWindowFlags_NoTitleBar);
-    {
-        ImGui::SetWindowPos(ImVec2(0, 22));
-        ImGui::SetWindowSize(ImVec2(leftWidth, 40));
-        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(5,15));
+            ImGui::SameLine();
+            ImGui::Text("  ");
+            ImGui::SameLine();
+            if (ImGui::Button(FontAwesomeIcons::FA_TACHOMETER, ImVec2(30, 30))) show_smartcontrols ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(FontAwesomeIcons::FA_SLIDERS, ImVec2(30, 30))) show_mixer ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(FontAwesomeIcons::FA_SCISSORS, ImVec2(30, 30))) show_editors ^= 1;
 
-        if (ImGui::Button(FontAwesomeIcons::FA_INBOX, ImVec2(30, 30))) show_library ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(FontAwesomeIcons::FA_INFO, ImVec2(30, 30))) show_inspector ^= 1;
-
-        ImGui::SameLine();
-        ImGui::Text("  ");
-        ImGui::SameLine();
-        if (ImGui::Button(FontAwesomeIcons::FA_SLIDERS, ImVec2(30, 30))) show_smartcontrols ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(FontAwesomeIcons::FA_SLIDERS, ImVec2(30, 30))) show_mixer ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(FontAwesomeIcons::FA_SCISSORS, ImVec2(30, 30))) show_editors ^= 1;
-
-        ImGui::PopStyleVar();
-    }
-    ImGui::End();
-
-    ImGui::Begin("TOOLBAR", &open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove |  ImGuiWindowFlags_NoTitleBar);
-    {
-        ImGui::SetWindowPos(ImVec2(leftWidth, 22));
-        ImGui::SetWindowSize(ImVec2(210, 40));
-        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(5,15));
-
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_FAST_REWIND, ImVec2(30, 30))) show_test_window ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_FAST_FORWARD, ImVec2(30, 30))) show_test_window ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_SKIP_NEXT, ImVec2(30, 30))) show_test_window ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_PLAY_ARROW, ImVec2(30, 30))) show_test_window ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_FIBER_MANUAL_RECORD, ImVec2(30, 30))) show_test_window ^= 1;
-
-        ImGui::PopStyleVar();
-    }
-    ImGui::End();
-
-    static bool show_listeditor = false;
-    static bool show_notes = false;
-    static bool show_loops = false;
-    static bool show_browser = false;
-    ImGui::Begin("TOOLBAR_RIGHT", &open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove |  ImGuiWindowFlags_NoTitleBar);
-    {
-        ImGui::SetWindowPos(ImVec2(this->_display_w - leftWidth, 22));
-        ImGui::SetWindowSize(ImVec2(leftWidth, 40));
-        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(5,15));
-
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_LIST, ImVec2(30, 30))) show_listeditor ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_NOTE, ImVec2(30, 30))) show_notes ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_ALL_INCLUSIVE, ImVec2(30, 30))) show_loops ^= 1;
-        ImGui::SameLine();
-        if (ImGui::Button(GoogleIcons::GI_LIBRARY_MUSIC, ImVec2(30, 30))) show_browser ^= 1;
-
-        ImGui::PopStyleVar();
-    }
-    ImGui::End();
-
-    auto v = ImVec2((float)this->_display_w, (float)this->_display_h - 62.0f);
-    ImGui::Begin("Splitter test2", &open, ImGuiWindowFlags_NoResize |ImGuiWindowFlags_NoMove |  ImGuiWindowFlags_NoTitleBar);
-    {
-        ImGui::SetWindowPos(ImVec2(0, 62));
-        ImGui::SetWindowSize(v);
-        ImGui::SetNextWindowSizeConstraints(v, v);
-
-        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0,0));
-
-        ImGui::BeginChild("child1", ImVec2(w, h), true);
-        // 1. Show a simple window
-        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
-        {
-            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(15,15));
-            static float f = 0.0f;
-            std::cout << "f: " << f << std::endl;
-            f = this->_mixer->Pvolume / 128.0f;
-            ImGui::SliderFloat("Volume", (float*)&(f), 0.0f, 1.0f);
-            this->_mixer->setPvolume((unsigned char)(f * 128));
-            ImGui::ColorEdit3("clear color", (float*)&clear_color);
-            if (ImGui::Button("Another Window")) show_another_window ^= 1;
             ImGui::PopStyleVar();
         }
-        ImGui::EndChild();
+        ImGui::End();
 
-        ImGui::SameLine();
-        ImGui::InvisibleButton("vsplitter", ImVec2(8.0f,h));
-        if (ImGui::IsItemActive())
-            w += ImGui::GetIO().MouseDelta.x;
-
-        ImGui::SameLine();
-
-        ImGui::BeginChild("child2", ImVec2(0, h), true);
+        ImGui::Begin("TOOLBAR", &open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove |  ImGuiWindowFlags_NoTitleBar);
         {
+            ImGui::SetWindowPos(ImVec2(leftWidth, 22));
+            ImGui::SetWindowSize(ImVec2(210, 40));
+            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(5,15));
 
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_FAST_REWIND, ImVec2(30, 30))) show_test_window ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_FAST_FORWARD, ImVec2(30, 30))) show_test_window ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_SKIP_NEXT, ImVec2(30, 30))) show_test_window ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_PLAY_ARROW, ImVec2(30, 30))) show_test_window ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_FIBER_MANUAL_RECORD, ImVec2(30, 30))) show_test_window ^= 1;
+
+            ImGui::PopStyleVar();
         }
-        ImGui::EndChild();
+        ImGui::End();
 
-        ImGui::InvisibleButton("hsplitter", ImVec2(-1,8.0f));
-        if (ImGui::IsItemActive())
-            h += ImGui::GetIO().MouseDelta.y;
-
-        ImGui::BeginChild("child3", ImVec2(0,0), true);
+        static bool show_listeditor = false;
+        static bool show_notes = false;
+        static bool show_loops = false;
+        static bool show_browser = false;
+        ImGui::Begin("TOOLBAR_RIGHT", &open, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoMove |  ImGuiWindowFlags_NoTitleBar);
         {
+            ImGui::SetWindowPos(ImVec2(this->_display_w - 150, 22));
+            ImGui::SetWindowSize(ImVec2(150, 40));
+            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(5,15));
 
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_LIST, ImVec2(30, 30))) show_listeditor ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_NOTE, ImVec2(30, 30))) show_notes ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_ALL_INCLUSIVE, ImVec2(30, 30))) show_loops ^= 1;
+            ImGui::SameLine();
+            if (ImGui::Button(GoogleIcons::GI_LIBRARY_MUSIC, ImVec2(30, 30))) show_browser ^= 1;
+
+            ImGui::PopStyleVar();
         }
-        ImGui::EndChild();
+        ImGui::End();
 
-        ImGui::PopStyleVar();
+        if (show_library)
+        {
+            ImGui::Begin("library", &show_library, ImGuiWindowFlags_NoResize |ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
+            {
+                ImGui::SetWindowPos(ImVec2(0, 62));
+                ImGui::SetWindowSize(ImVec2(150, this->_display_h));
+
+                ImGui::Text("Library");
+            }
+            ImGui::End();
+        }
+
+        if (show_inspector)
+        {
+            ImGui::Begin("inspector", &show_inspector, ImGuiWindowFlags_NoResize |ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoTitleBar);
+            {
+                ImGui::SetWindowPos(ImVec2(show_library ? 150 : 0, 62));
+                ImGui::SetWindowSize(ImVec2(150, this->_display_h));
+
+                ImGui::Text("Inspector");
+            }
+            ImGui::End();
+        }
+
+        auto v = ImVec2((float)this->_display_w - (show_library ? 150 : 0) - (show_inspector ? 150 : 0), (float)this->_display_h - 62.0f);
+        ImGui::Begin("Splitter test2", &open, ImGuiWindowFlags_NoResize |ImGuiWindowFlags_NoMove |  ImGuiWindowFlags_NoTitleBar);
+        {
+            ImGui::SetWindowPos(ImVec2((show_library ? 150 : 0) + (show_inspector ? 150 : 0), 62));
+            ImGui::SetWindowSize(v);
+            ImGui::SetNextWindowSizeConstraints(v, v);
+
+            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0,0));
+
+            ImGui::BeginChild("child1", ImVec2((show_library ? 150 : 0) + (show_inspector ? 150 : 0), h), true);
+            {
+                ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(15,15));
+                static float f = 0.0f;
+                f = this->_mixer->Pvolume / 128.0f;
+                ImGui::SliderFloat("Volume", (float*)&(f), 0.0f, 1.0f);
+                this->_mixer->setPvolume((unsigned char)(f * 128));
+                ImGui::ColorEdit3("clear color", (float*)&clear_color);
+                if (ImGui::Button("Another Window")) show_another_window ^= 1;
+                ImGui::PopStyleVar();
+            }
+            ImGui::EndChild();
+
+            ImGui::InvisibleButton("hsplitter", ImVec2(-1,8.0f));
+            if (ImGui::IsItemActive())
+                h += ImGui::GetIO().MouseDelta.y;
+
+            ImGui::BeginChild("child3", ImVec2(0,0), true);
+            {
+
+            }
+            ImGui::EndChild();
+
+            ImGui::PopStyleVar();
+        }
+        ImGui::End();
     }
-    ImGui::End();
+    ImGui::PopStyleColor();
     ImGui::PopStyleVar();
 
     // Rendering
